7ade5d87a42c46c1291a2926d446fb7a039f129a
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2017-03-06 23:51:42 +0530

    Complete Project 3

--- jQueryYoutubeSearchEngine/js/script.js
+++ jQueryYoutubeSearchEngine/js/script.js
@@ -68,6 +68,94 @@ function search(){
     );
 }
 
+//Next page function
+
+function nextPage() {
+
+    var token = $('#next-button').data('token');
+    var query = $('#next-button').data('query');
+
+    // Clear Results
+    $('#results').html('');
+    $('#buttons').html('');
+
+    // Get Form Input
+    q = $('#query').val();
+
+    // Run GET Request on API
+    $.get(
+        "https://www.googleapis.com/youtube/v3/search",{
+            part: 'snippet, id',
+            q: q,
+            pageToken: token,
+            type:'video',
+            key: ''},
+        function(data){
+            var nextPageToken = data.nextPageToken;
+            var prevPageToken = data.prevPageToken;
+
+            // Log Data
+            console.log(data);
+
+            $.each(data.items, function(i, item){
+                // Get Output
+                var output = getOutput(item);
+
+                // Display Results
+                $('#results').append(output);
+            });
+
+            var buttons = getButtons(prevPageToken, nextPageToken);
+
+            $('#buttons').append(buttons);
+        }
+    );
+}
+
+//Prev page function
+
+function prevPage() {
+
+    var token = $('#prev-button').data('token');
+    var query = $('#prev-button').data('query');
+
+    // Clear Results
+    $('#results').html('');
+    $('#buttons').html('');
+
+    // Get Form Input
+    q = $('#query').val();
+
+    // Run GET Request on API
+    $.get(
+        "https://www.googleapis.com/youtube/v3/search",{
+            part: 'snippet, id',
+            q: q,
+            pageToken: token,
+            type:'video',
+            key: ''},
+        function(data){
+            var nextPageToken = data.nextPageToken;
+            var prevPageToken = data.prevPageToken;
+
+            // Log Data
+            console.log(data);
+
+            $.each(data.items, function(i, item){
+                // Get Output
+                var output = getOutput(item);
+
+                // Display Results
+                $('#results').append(output);
+            });
+
+            var buttons = getButtons(prevPageToken, nextPageToken);
+
+            $('#buttons').append(buttons);
+        }
+    );
+}
+
 function getOutput(item) {
     var videoId = item.id.videoId;
     var title = item.snippet.title;
@@ -93,8 +181,16 @@ function getOutput(item) {
 
 function getButtons(prevPageToken, nextPageToken) {
     if(!prevPageToken) {
-        var btnoutput = '<div class="button-container"' +
-        '<button id="next-button" class="paging-button" data-token="' + nextPageToken +'" data-qu';
-
+        var btnoutput = '<div class="button-container">' +
+        '<button id="next-button" class="paging-button" data-token="' + nextPageToken +'" data-query="'+q+'"' +
+                            'onclick="nextPage();">Next Page</button></div>';
+    } else {
+        var btnoutput = '<div class="button-container">' +
+                '<button id="prev-button" class="paging-button" data-token="' + prevPageToken +'" data-query="'+q+'"' +
+        'onclick="prevPage();">Prev Page</button>' +
+            '<button id="next-button" class="paging-button" data-token="' + nextPageToken +'" data-query="'+q+'"' +
+        'onclick="nextPage();">Next Page</button></div>';
     }
+
+    return btnoutput;
 }
